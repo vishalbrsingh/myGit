ed75ab8e7d8f0dfcb198bc6e42f95d4e94615d7d
vishalbrsingh <vishalbrsingh@gmail.com>
2022-02-28 01:54:36 +0530

    html and JS file of resume builder added

---
+++ Resume using Bootstrap/resumeData.js
@@ -0,0 +1,80 @@
+const personalData = {
+    name: "Xyz xyz",
+    dob: "dd-mm-yyyy",
+    place: "xyz",
+    mobile: "9876543210",
+    email: "Xyz@gmail.com",
+    linkedIn : "https://www.linkedin.com/in/Xyz/"
+}
+
+// Objective
+const objective = "xyz"
+
+// work Experience - latest to oldest
+const company =  [
+    {
+        companyName: "xyz",
+        designation: "xyz",
+        description: "xyz",
+        from : "Month 20XX",
+        to : "Month 20XX"
+    }]
+
+// Education - latest to oldest
+const school = [
+    {
+        level : "Bachelor of xyz",
+        stream : "xyz",
+        schoolName : "xyz",
+        board : "University of xyz",
+        from : "YYYY",
+        year : "YYYY"
+    },
+    {
+        level : "XII",
+        stream : "xyz",
+        schoolName : "xyz",
+        board : "xyz",
+        from : "YYYY",
+        year : "YYYY"
+    },
+    {
+        level : "X",
+        stream : "xyz",
+        schoolName : "xyz",
+        board : "xyz",
+        from : "YYYY",
+        year : "YYYY"
+    }
+]
+
+// Project - latest to oldest
+const project =  [
+// {
+//     title: "",
+//     platform: "",
+//     description: "",
+//     duration : ""
+// },
+{
+    title: "xyz",
+    platform: "xyz",
+    description: "xyz",
+    duration : "N months"
+}]
+
+// Skills
+const skills = ["xyz", "xyz", "xyz", "xyz", "xyz"]
+
+// Other Details
+const otherDetails = {
+    nationality : "Indian",
+    gender : "Male",
+    language : "English, Hindi",
+    passport : "Available",
+}
+
+// Extra Curricular Activities
+const cca = [
+    "xyz","xyz"
+]
